c7a88e117c829cfab149807b620d7192db3af660
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-07-01 20:52:26 -0500

    Add files via upload

---
+++ 01-app.js
@@ -0,0 +1,34 @@
+//Inicializar una variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+console.log(producto);
+
+//Las vairables se puede reasignar
+producto ='Monitor de 19 Pulgadas';
+console.log(producto);
+
+//Javascript es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto=20;
+console.log(producto);
+
+
+//Se pueden inicializar sin valor y asignarlo despues
+var disponible;
+disponible = true;
+
+disponible = false;
+
+//Inicializar multiples variables
+var categoria = 'Computadoras',
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+//Las variables no pueden iniciar con numero
+
+var 99dias;
+var dias99;
+
+var 01_;
+var_01;
+
+var nombreProductoCategoriaPrecio;
+var nombre_producto_categoria_precio;
