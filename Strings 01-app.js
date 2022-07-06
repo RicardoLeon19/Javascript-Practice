efc1ba49ec58b8b5b2b621e93a643878f82741b6
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-07-05 21:42:25 -0500

    Add files via upload

---
+++ Strings 01-app.js
@@ -0,0 +1,9 @@
+const producto = 'Monitor 20 pulgadas';
+const producto2 = String ('Monitor 20 pulgadass');
+
+const producto3 = new String ('Monitor 27 pulgadas');
+
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
