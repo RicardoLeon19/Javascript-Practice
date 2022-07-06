7655bc923a11df6d2962c689ce1972807e5034cd
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-07-05 21:42:34 -0500

    Add files via upload

---
+++ Strings 02-app.js
@@ -0,0 +1,17 @@
+const producto = 'Monitor 20 pulgadas';
+
+
+//Conocer la cantidad de letras de cadenas de texto
+console.log(producto)
+console.log(producto.length);
+
+
+
+
+//console.log(producto.indexOf('pulgada'));
+
+
+console.log(producto.includes('Tablet'));
+console.log(producto.indexOf('Monitor'));
+
+
