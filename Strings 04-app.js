4dd6006619d096769c88c0e3064ae58177a257cb
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-07-08 21:30:09 -0500

    Add files via upload

---
+++ Strings 04-app.js
@@ -0,0 +1,11 @@
+const producto = '                       Monitor 20 pulgadas                  ';
+console.log(producto.length);
+
+//Eliminiar del inicio
+console.log(producto.trimStart());
+console.log(producto.trimEnd());
+
+console.log(producto.trimStart().trimEnd());
+
+
+console.log(producto.trim());
