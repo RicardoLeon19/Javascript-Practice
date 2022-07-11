a11bf4187b5c8b12ae3b63823c4f62c4eb5fdb22
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-07-10 19:38:58 -0500

    Add files via upload

---
+++ strings 07-app.js
@@ -0,0 +1,14 @@
+const producto = 'Monitor 20 pulgadas';
+
+console.log(producto.toUpperCase());
+
+console.log(producto.toLowerCase());
+
+
+const email = "Correo@Correo.com";
+console.log (email.toLowerCase());
+
+
+const precio =300;
+console.log(precio);
+console.log(precio.toString());
