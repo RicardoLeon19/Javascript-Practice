1df97eb5b521887f76e9451b638793637594288f
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-07-11 22:32:52 -0500

    Add files via upload

---
+++ Numeros 03-app.js
@@ -0,0 +1,44 @@
+let resultado;
+
+//PI
+
+resultado = Math.PI;
+
+//Redondear
+
+resultado = Math.round (2.8);
+
+//Redondear hacia arriba
+
+resultado = Math.ceil(2.1);
+
+//Redondear hacia abajo
+resultado = Math.floor(2.9);
+
+//Raiz cuadrada 
+resultado = Math.sqrt(144);
+
+//Absoluto
+resultado = Math.abs(-500);
+
+//Potencia
+resultado = Math.pow(2,4);
+
+//Minimo
+resultado = Math.min(3,5,1,12,-3);
+
+//Maximo
+resultado =Math.max(2,4,6,7);
+
+//Aleatorio
+resultado = Math.random();
+
+//Aleatorio dentro de un rango...
+
+resultado = Math.floor(Math.random() *30);
+
+
+
+
+
+console.log(resultado);
