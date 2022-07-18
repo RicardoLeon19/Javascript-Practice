643bf9ef08fe628cfc60e4cc6110aedb8bc670b3
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-07-18 16:41:27 -0500

    Add files via upload

---
+++ Array 01-app.js
@@ -0,0 +1,11 @@
+const numero = [10,20,30];
+
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+
+
+console.log(meses);
+
+
+//Un arreglo que contiene datos de todo tipo
+
+const deTodo = ['Hola', 10, true, 'si', {nombre: 'juan', trabajo: 'Programador'}, [1,2,3]];
