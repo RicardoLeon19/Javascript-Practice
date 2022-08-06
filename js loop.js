b0540643174f3c08f56357ee950b0acef31a5791
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-08-06 18:26:23 -0500

    Add files via upload

---
+++ js loop.js
@@ -0,0 +1,11 @@
+for (var i = 5; i > 0; i--) {
+    console.log(i);
+};
+console.log('Countdown finished!');
+
+var i = 5;
+while (i > 0) {
+    console.log(i);
+    i = i - 1;
+};
+console.log('Counting completed!');
