5e4e897da906cec436a17842235f28f5a898ce7e
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-08-05 19:44:14 -0500

    Add files via upload

---
+++ js 5.js
@@ -0,0 +1,35 @@
+var day = "monday";
+
+switch(day){
+    case 'monday':
+        console.log("Do a bbqs");
+        break;
+    
+    case 'tuesday':
+        console.log("Do Sing");
+        break;
+
+    case 'wednesday':    
+        console.log("Do EAt");
+         break;    
+
+    case 'thursday':    
+        console.log("Do Run");
+        break;      
+
+    case 'friday':    
+    console.log("Do Swimming");
+        break;  
+
+    case 'saturday':    
+    console.log("Do Parkour");
+        break;  
+
+    case 'sunday':    
+    console.log("Do Yoga");
+        break;          
+        
+    default:
+        console.log('There is no such day')
+}
+
