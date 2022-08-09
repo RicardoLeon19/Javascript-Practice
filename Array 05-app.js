8e92bb8bd83b34bf8f8893b51c7e5262395ca121
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-08-08 22:02:41 -0500

    Add files via upload

---
+++ Array 05-app.js
@@ -0,0 +1,17 @@
+const carrito = [];
+
+// DEfinir un producto
+
+const producto = {
+    nombre: 'Monitor 32 pulgadas',
+    precio: 400
+}
+const producto2 ={
+    nombre: 'Celular',
+    precio: 800
+}
+
+//.op
+carrito.push(producto);
+
+console.log(carrito);
