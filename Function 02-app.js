3602a478ae5808035aa8bdfc2ebdbe24de936dfe
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-08-11 00:17:37 -0500

    Add files via upload

---
+++ Function 02-app.js
@@ -0,0 +1,17 @@
+//Declaracion de funcion (function Declaration)
+
+sumar();
+function sumar() {
+    console.log(2+2);
+}
+
+
+
+//Expresion de Funcion (Function Expression)
+
+sumar2();
+
+const sumar2 = function(){
+    console.log(3+3);
+}
+
