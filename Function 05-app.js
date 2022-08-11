ffd0aeb98312cebfad2af597e493f8eb659050d8
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-08-11 12:00:53 -0500

    Add files via upload

---
+++ Function 05-app.js
@@ -0,0 +1,16 @@
+function sumar (a,b){ //a y b son parametros
+    console.log(a+b);
+}
+
+sumar(2,3);
+
+sumar(200,184)
+
+
+
+function saludar(nombre, apellido) {
+    console.log(`Hola ${nombre} ${apellido}`)
+}
+
+
+saludar('Juan', 'De la Torre');
