b01ceb4740a79198b55697311168a3fe6c68be1e
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-08-20 15:38:07 -0500

    Add files via upload

---
+++ Function 10-app.js
@@ -0,0 +1,8 @@
+const aprendiendo = function(tecnologia){
+    console.log(`Aprendiendo ${tecnologia}`);
+}
+aprendiendo('Javascript')
+
+const aprendiendo2 = (tecnologia) => `Aprendiendo ${tecnologia}`;
+
+console.log(aprendiendo('Javascript'));
