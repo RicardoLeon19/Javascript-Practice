fbdb7c42e59100489ea0df274b304834f7081139
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-08-22 15:49:25 -0500

    Add files via upload

---
+++ Structures 02-app.js
@@ -0,0 +1,16 @@
+
+const puntaje = 1000;
+const puntaje2 = "1000";
+
+//if(puntaje == 1000) { //!= diferente == igual ag
+//    console.log('si es igual...')
+// }
+
+if( puntaje !== "1000"){ //!= diferente == igual a
+    console.log('No es igual')
+} else {
+    console.log('no es igual')
+}
+
+// == Operador que no es estricto
+//=== Operador Estricto
