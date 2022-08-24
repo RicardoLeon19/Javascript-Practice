8f00389043f3ed6953d1f2ead22391f2e95d7499
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-08-23 23:21:52 -0500

    Add files via upload

---
+++ Variable 05.js
@@ -0,0 +1,18 @@
+// Switch Case 
+
+const metodoPago = 'tarjeta';
+
+switch(metodoPago){
+    case 'efectivo':
+        console.log(`Pagaste con ${metodoPago}`);
+        break;
+    case 'cheque':
+        console.log(`Pagaste con ${metodoPago}`);
+        break;
+    case 'tarjeta':
+        console.log(`Pagaste con ${metodoPago}`);
+        break;
+    default:
+            console.log('Aun no has seleccionado un emtodo de pago');
+        break;
+}
