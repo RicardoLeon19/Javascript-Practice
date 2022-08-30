6cc0f1f5029ae4c2720872920e439e022c9e2a53
Ricardo León <108110183+RicardoLeon19@users.noreply.github.com>
2022-08-29 23:18:55 -0500

    Add files via upload

---
+++ Iterations 01-app.js
@@ -0,0 +1,11 @@
+//for (let i = 0; i < 10; i++ ) {
+//  console.log(`Numero ${i}` )
+//}
+
+for(let i=0; i<=20; i++){
+    if(i % 2 === 0) {
+        console.log(`El numero ${i} es PAR`);
+    }   else{
+        console.log(`El numero ${i} es IMPAR`)
+    }
+}
